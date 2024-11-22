28d948f2904bd4dac297d154d9e2ccd9cc1145c3
ksb_zyt_git <1017415870@qq.com>
2024-11-22 16:25:47 +0800

    add demo

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:app/video_demo.dart';
 import 'package:flutter/material.dart';
-import 'live.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LivePage(),
+      home: const VideoDemo(),
     );
   }
 }
--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import video_compress
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  VideoCompressPlugin.register(with: registry.registrar(forPlugin: "VideoCompressPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
