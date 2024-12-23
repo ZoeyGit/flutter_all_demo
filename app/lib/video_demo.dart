c51dd769839f9c5bf9c4eafa10e05a42824562ca
ksb_zyt_git <1017415870@qq.com>
2024-12-23 16:39:46 +0800

    1

--- app/lib/video_demo.dart
+++ app/lib/video_demo.dart
@@ -1,7 +1,5 @@
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:video_compress/video_compress.dart';
-import 'package:video_player/video_player.dart';
 
 class VideoDemo extends StatefulWidget {
   const VideoDemo({super.key});
@@ -11,7 +9,7 @@ class VideoDemo extends StatefulWidget {
 }
 
 class _VideoDemoState extends State<VideoDemo> {
-  late VideoPlayerController _controller;
+  // late VideoPlayerController _controller;
   Future<void>? _initializeVideoPlayerFuture;
 
   String videopath =
@@ -20,27 +18,27 @@ class _VideoDemoState extends State<VideoDemo> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(
-      Uri.parse(videopath),
-    );
-    getVideoThumbnail();
-    _initializeVideoPlayerFuture = _controller.initialize().then((_) {
-      // 确保获取第一帧
-      _controller.setVolume(0.0);
-      _controller.seekTo(Duration.zero);
-      _controller.setLooping(true);
-      setState(() {});
-    });
+    // _controller = VideoPlayerController.networkUrl(
+    //   Uri.parse(videopath),
+    // );
+    // getVideoThumbnail();
+    // _initializeVideoPlayerFuture = _controller.initialize().then((_) {
+    //   // 确保获取第一帧
+    //   _controller.setVolume(0.0);
+    //   _controller.seekTo(Duration.zero);
+    //   _controller.setLooping(true);
+    //   setState(() {});
+    // });
   }
 
   void getVideoThumbnail() async {
-    final uint8list = await VideoCompress.getByteThumbnail(videopath,
-        quality: 50, // default(100)
-        position: -1 // default(-1)
-        );
-    setState(() {
-      this.uint8list = uint8list;
-    });
+    // final uint8list = await VideoCompress.getByteThumbnail(videopath,
+    //     quality: 50, // default(100)
+    //     position: -1 // default(-1)
+    //     );
+    // setState(() {
+    //   this.uint8list = uint8list;
+    // });
   }
 
   @override
@@ -49,71 +47,71 @@ class _VideoDemoState extends State<VideoDemo> {
       appBar: AppBar(
         title: const Text('视频播放器'),
       ),
-      body: Center(
-        child: FutureBuilder(
-          future: _initializeVideoPlayerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    VideoPlayer(_controller),
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _controller.value.isPlaying
-                              ? _controller.pause()
-                              : _controller.play();
-                        });
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.black26,
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Icon(
-                          _controller.value.isPlaying
-                              ? Icons.pause
-                              : Icons.play_arrow,
-                          size: 50,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            } else {
-              // 加载时显示占位图或加载指示器
-              return AspectRatio(
-                aspectRatio: 16 / 9,
-                child: Stack(
-                  children: [
-                    if (uint8list != null)
-                      Image.memory(
-                        uint8list!,
-                        fit: BoxFit.cover,
-                      )
-                    else
-                      Container(color: Colors.black),
-                    const Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  ],
-                ),
-              );
-            }
-          },
-        ),
-      ),
+      // body: Center(
+      //   child: FutureBuilder(
+      //     future: _initializeVideoPlayerFuture,
+      //     builder: (context, snapshot) {
+      //       if (snapshot.connectionState == ConnectionState.done) {
+      //         return AspectRatio(
+      //           aspectRatio: _controller.value.aspectRatio,
+      //           child: Stack(
+      //             alignment: Alignment.center,
+      //             children: [
+      //               VideoPlayer(_controller),
+      //               GestureDetector(
+      //                 onTap: () {
+      //                   setState(() {
+      //                     _controller.value.isPlaying
+      //                         ? _controller.pause()
+      //                         : _controller.play();
+      //                   });
+      //                 },
+      //                 child: Container(
+      //                   decoration: BoxDecoration(
+      //                     color: Colors.black26,
+      //                     borderRadius: BorderRadius.circular(30),
+      //                   ),
+      //                   child: Icon(
+      //                     _controller.value.isPlaying
+      //                         ? Icons.pause
+      //                         : Icons.play_arrow,
+      //                     size: 50,
+      //                     color: Colors.white,
+      //                   ),
+      //                 ),
+      //               ),
+      //             ],
+      //           ),
+      //         );
+      //       } else {
+      //         // 加载时显示占位图或加载指示器
+      //         return AspectRatio(
+      //           aspectRatio: 16 / 9,
+      //           child: Stack(
+      //             children: [
+      //               if (uint8list != null)
+      //                 Image.memory(
+      //                   uint8list!,
+      //                   fit: BoxFit.cover,
+      //                 )
+      //               else
+      //                 Container(color: Colors.black),
+      //               const Center(
+      //                 child: CircularProgressIndicator(),
+      //               ),
+      //             ],
+      //           ),
+      //         );
+      //       }
+      //     },
+      //   ),
+      // ),
     );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    // _controller.dispose();
     super.dispose();
   }
 }
--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import video_compress
-import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  VideoCompressPlugin.register(with: registry.registrar(forPlugin: "VideoCompressPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
